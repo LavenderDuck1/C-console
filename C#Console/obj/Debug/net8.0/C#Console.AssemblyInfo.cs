80935bdc7045cae30dae8a9f34bfa2cbbfc746d3
Diego Mendez <25mendezde@marshallisd.com>
2024-09-24 13:42:20 -0500

    Calculator

--- C#Console/obj/Debug/net8.0/C#Console.AssemblyInfo.cs
+++ C#Console/obj/Debug/net8.0/C#Console.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#Console")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c200a96f162a7e9da2d6295f088b5a69b7a9a96")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#Console")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#Console")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
